683b9b0341d40a79ddf5e240ec55df274b7e361f
vancore295 <silverfire295@gmail.com>
2020-12-26 17:53:04 -0500

    first
    first

---
+++ src/main/java/com/example/eurekatest/eurekatest/EurekatestApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurekatest.eurekatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekatestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekatestApplication.class, args);
+    }
+
+}
